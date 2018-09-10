9ee7c6d2c8afadd9303923349b979cbae32c83ae
kornienko199004 <kornienko199004@gmail.com>
2018-09-10 11:26:11 +0300

    Task 3

--- server/index.js
+++ server/index.js
@@ -1,3 +1,7 @@
+// @flow
+
+// import 'babel-polyfill';
+
 import path from 'path';
 import Koa from 'koa';
 import Pug from 'koa-pug';
@@ -5,7 +9,7 @@ import socket from 'socket.io';
 import http from 'http';
 import Router from 'koa-router';
 import koaLogger from 'koa-logger';
-import serve from 'koa-static';
+// import serve from 'koa-static';
 import middleware from 'koa-webpack';
 import bodyParser from 'koa-bodyparser';
 import session from 'koa-generic-session';
@@ -21,13 +25,9 @@ export default () => {
   app.use(session(app));
   app.use(bodyParser());
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(serve(path.join(__dirname, '..', 'dist', 'assets')));
-  } else {
-    app.use(middleware({
-      config: webpackConfig,
-    }));
-  }
+  app.use(middleware({
+    config: webpackConfig,
+  }));
 
   const router = new Router();
 
